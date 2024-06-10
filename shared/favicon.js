fdab70b24ce11fff35806cbd15734ae1cd7c81b7
Refil <53364591+vRefilv@users.noreply.github.com>
2024-06-10 19:15:51 +0200

    upload

---
+++ shared/favicon.js
@@ -0,0 +1,28 @@
+function updateFavicon(theme) {
+        const favicon = document.getElementById('favicon');
+        if (theme === 'dark') {
+            favicon.href = 'crossed-ok-icon_white.webp';
+        } else {
+            favicon.href = 'crossed-ok-icon_black.webp';
+        }
+    }
+
+    function setFavicon() {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            updateFavicon('dark');
+        } else {
+            updateFavicon('light');
+        }
+
+        // Listen for changes in the color scheme preference
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+            if (event.matches) {
+                updateFavicon('dark');
+            } else {
+                updateFavicon('light');
+            }
+        });
+    }
+
+    // Call the function to set the favicon on page load
+    setFavicon();
