dedf0b7d343b3ff59847b16d38428bed5764581a
Refil <53364591+vRefilv@users.noreply.github.com>
2024-07-10 20:00:22 +0200

    update

---
+++ nightly/bin/cards.js
@@ -0,0 +1,15 @@
+$('.example').plate({
+    perspective: 125,
+    maxRotation: 5,
+    animationDuration: 200
+});
+$('.examples2').plate({
+    perspective: 250,
+    maxRotation: 5,
+    animationDuration: 200
+});
+$('footer').plate({
+    perspective: 250,
+    maxRotation: 5,
+    animationDuration: 200
+});
