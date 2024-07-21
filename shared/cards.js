f0e0c841f0282bfe75dfa5354436e26fbd141ead
Refil <53364591+vRefilv@users.noreply.github.com>
2024-07-21 17:50:33 +0200

    update & fixes

---
+++ shared/cards.js
@@ -0,0 +1,17 @@
+if ($(window).width() > 600) {
+$('.example').plate({
+    perspective: 125,
+    maxRotation: 5,
+    animationDuration: 200
+});
+$('.examples').plate({
+    perspective: 250,
+    maxRotation: 5,
+    animationDuration: 200
+});
+$('footer').plate({
+    perspective: 250,
+    maxRotation: 5,
+    animationDuration: 200
+});
+}
---
+++ shared/cursor.js
@@ -0,0 +1,6 @@
+if ($(window).width() > 600) {
+window.addEventListener("load", (event) => {
+  new cursoreffects.followingDotCursor({ color: ["#323232a6"] });
+  });
+}
+  
